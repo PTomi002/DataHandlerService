fd254091880428604c14971b481f4a6ade41a997
Tamas David Paulin <tamas.paulin@evosoft.com>
2018-06-27 16:14:45 +0200

    add compose file

--- src/main/java/hu/exercise/microservice/DataHandlerServer/client/DataHandlerClient.java
+++ src/main/java/hu/exercise/microservice/DataHandlerServer/client/DataHandlerClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(value = "data-handler", fallback = DataHandlerClientFallback.class)
+@FeignClient(value = "datahandler", fallback = DataHandlerClientFallback.class)
 public interface DataHandlerClient {
 
     @GetMapping("/{id}")
